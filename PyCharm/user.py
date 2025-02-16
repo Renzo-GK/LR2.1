5362713702fbef83f52fee0ffae961e1ea83cc97
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-17 02:47:08 +0300

    created user.py

---
+++ PyCharm/user.py
@@ -0,0 +1,7 @@
+name = str(input("What is  your name?\n"))
+age = int(input("How old are you?\n"))
+live_in = input("Where are you live?\n")
+
+print("This is", name)
+print("It is", age)
+print("(S)he live in", live_in)
