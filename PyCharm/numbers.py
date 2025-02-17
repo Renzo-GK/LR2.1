c78cab6ff67e7760a0c4e811a81feff64c6d3626
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-17 03:06:49 +0300

    created numbers.py

---
+++ PyCharm/numbers.py
@@ -0,0 +1,12 @@
+print("Please enter some numbers:")
+number_1 = int(input("Number 1:"))
+number_2 = int(input("Number 2:"))
+number_3 = int(input("Number 3:"))
+number_4 = int(input("Number 4:"))
+
+summ_1 = number_1 + number_2
+summ_2 = number_3 + number_4
+
+result = summ_1/summ_2
+
+print("{:.2f}".format(result))
