1d6761b0a3b5452d991356b11ab3f01fbf573a04
Omaraschab Guseinov <gk.kobayashi.sss@gmail.com>
2025-02-17 03:20:12 +0300

    last commit

---
+++ PyCharm/individual.py
@@ -0,0 +1,9 @@
+a = float(input("Side a:\n"))
+b = float(input("Side b:\n"))
+h = float(input("Side h:\n"))
+
+volume = float(a*b*h)
+
+side_surface_area = float(2*h*(a+b))
+
+print(volume, side_surface_area)
